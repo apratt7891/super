6f87c1cdf22969c39a1fae4a0f54235663ee7ea5
apratt7891 <apratt7891@gmail.com>
2021-04-12 20:45:33 -0600

    add modal

--- Develop/calendar.js
+++ Develop/calendar.js
@@ -2,3 +2,7 @@ function displayDate() {
     document.getElementById("currentDay").innerHTML =
     Date();
   }
+  $('.modal-container').toggle()
+  $('.modal-conter--close').click(function() {
+      $('.modal-container').css('display', 'none')
+  })
