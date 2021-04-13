e47e079507d479fb29e5a3725e0c59245c11b7d1
apratt7891 <apratt7891@gmail.com>
2021-04-12 21:21:24 -0600

    remove modal

--- Develop/calendar.js
+++ Develop/calendar.js
@@ -1,8 +1,2 @@
-function displayDate() {
-    document.getElementById("currentDay").innerHTML =
-    Date();
-  }
-  $('.modal-container').toggle()
-  $('.modal-conter--close').click(function() {
-      $('.modal-container').css('display', 'none')
-  })
+document.getElementById("currentDay").innerHTML =
+ Date();
