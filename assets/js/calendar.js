86fee12f6120b7245c493bccbd55169a4d69404c
apratt7891 <apratt7891@gmail.com>
2021-04-17 21:34:42 -0600

    create folders

---
+++ assets/js/calendar.js
@@ -0,0 +1,23 @@
+document.getElementById("currentDay").innerHTML =
+ Date();
+
+
+
+$(".saveBtn").on("click", function() {
+
+    var saveTask = $("textarea")
+    .val();
+    localStorage.setItem("task", JSON.stringify(task));
+   
+});
+
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
